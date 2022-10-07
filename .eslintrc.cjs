17395d93d4da678d7f9b7258f05166c83d93e170
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-07 13:16:27 +0200

    eslint/prettier apply

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,44 +1,45 @@
 module.exports = {
     env: {
         es2021: true,
-        node: true
+        node: true,
     },
     extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
+    project: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
+            jsx: true,
         },
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: "./tsconfig.json"
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
     },
-    plugins: ["react", "import", "@typescript-eslint", "prettier"],
+    plugins: ['react', 'import', '@typescript-eslint', 'prettier'],
     rules: {
-        "react/jsx-indent": ["warn", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single"],
-        semi: ["error", "never"],
-        "prettier/prettier": "warn",
-        "import/no-unresolved": "error",
-        "@typescript-eslint/no-non-null-assertion": "off"
+        'react/jsx-indent': ['warn', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'prettier/prettier': 'warn',
+        'import/no-unresolved': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'off',
     },
     settings: {
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"]
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
         },
-        "import/resolver": {
+        'import/resolver': {
             typescript: {
                 alwaysTryTypes: true,
-                project: "./tsconfig.json"
-            }
+                project: './tsconfig.json',
+            },
         },
         react: {
-            version: "detect"
-        }
-    }
+            version: 'detect',
+        },
+    },
 }
