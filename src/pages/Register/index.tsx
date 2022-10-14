918661bbe0e68f211a474293b86033a79c845031
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-14 13:51:40 +0200

    style my register page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -26,10 +26,10 @@ const Register: React.FC = () => {
     }
 
     return (
-        <div className="w-screen h-100 relative bg-slate-900 text-p-2 text-base transition-colors">
-            <div className="w-90 h-90 flex flex-col items-center justify-center">
-                <div className="text-center w-150 h-50 z-10 bg-blue-900 p-10 rounded-lg text-slate-200">
-                    <div className="flex flex-col justify-center items-center mb-10 font-bold text-2xl select-none">
+        <div className="relative w-screen h-full bg-slate-900 text-p-2 text-base transition-colors">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center">
+                <div className="p-10 text-center z-10 bg-blue-900 rounded-lg text-slate-200">
+                    <div className="flex flex-col justify-center items-center mb-5 font-bold text-2xl select-none">
                         <div>
                             <span className="mr-2">Pi'Notes</span>
                             📌
@@ -40,25 +40,30 @@ const Register: React.FC = () => {
                             label="Identifiant"
                             onChange={e => setUserRegister({ ...userRegister, username: e })}
                             size="large"
+                            className="mb-4"
                         />
                         <Input
                             label="Mot de passe"
                             type="password"
                             onChange={e => setUserRegister({ ...userRegister, password: e })}
                             size="large"
+                            className="mb-4"
                         />
                         <Input
                             label="Confirmation de votre mot de passe"
                             type="password"
                             onChange={e => setUserRegister({ ...userRegister, confirmPassword: e })}
                             size="large"
+                            className="mb-4"
                         />
                         <div className="text-red-800 mb-5">{error}</div>
-                        <Button
-                            isLoading={isLoading}
-                            onClick={(e: any) => handleRegister(e)}
-                            title="M'inscrire"
-                        />
+                        <div className="mb-5">
+                            <Button
+                                isLoading={isLoading}
+                                onClick={(e: any) => handleRegister(e)}
+                                title="M'inscrire"
+                            />
+                        </div>
 
                         <Button
                             onClick={() => navigate(urls.APP.LOGIN)}
