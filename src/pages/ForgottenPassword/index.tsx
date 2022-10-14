de81a179d2148d58700e4a63089740d38c85fce1
Melly Lucas <melly.lucas32@yahoo.fr>
2022-10-14 13:04:18 +0200

    imgreturnforgottenpassword

--- src/pages/ForgottenPassword/index.tsx
+++ src/pages/ForgottenPassword/index.tsx
@@ -8,6 +8,8 @@ import Button from '../../components/Button'
 import api from '../../helpers/api'
 import { urls } from '../../helpers/urls'
 
+import RetourImg from '../../assets/images/retour.png'
+
 const ForgottenPassword: React.FC = () => {
     const navigate = useNavigate()
     const [isLoading, setIsLoading] = useState(false)
@@ -37,38 +39,50 @@ const ForgottenPassword: React.FC = () => {
 
     return (
         <div className="w-screen h-screen relative bg-slate-900 text-p-2 text-base transition-colors">
-            <div className="w-full h-full flex flex-col items-center justify-center ">
-                <div className="flex flex-col justify-center items-center mb-10 font-bold text-2xl select-none">
-                    <div>
-                        <span className="mr-2 text-slate-200">Mot de passe oublié ?</span>
-                    </div>
-                </div>
-                <div>
-                    <p className="text-slate-200 mb-20 mt-10">
-                        Indiquez votre adresse e-mail pour recevoir un message permettant d'obtenir
-                        votre mot de passe
-                    </p>
-                </div>
-                <form className="flex flex-col" onSubmit={handleForgottenPassword as any}>
-                    <div className="flex flex-nowrap">
-                        <div className="mr-10">
-                            <Input
-                                onChange={e => setIdentifer(e)}
-                                placeholder="john.doe@pinotes.com"
-                                size="large"
-                            />
+            <div>
+                <img
+                    className="p-8 ..."
+                    src={RetourImg}
+                    alt="Retour"
+                    onClick={() => {
+                        navigate(urls.APP.LOGIN)
+                    }}
+                />
+                <div className="mt-20 pt-10 ...">
+                    <div className="w-full h-full flex flex-col items-center justify-center">
+                        <div className="flex flex-col justify-center items-center mb-10 font-bold text-2xl select-none">
+                            <div>
+                                <span className="mr-2 text-slate-200">Mot de passe oublié ?</span>
+                            </div>
                         </div>
                         <div>
-                            <Button
-                                isLoading={isLoading}
-                                onClick={(e: any) => handleForgottenPassword(e)}
-                                title="Envoyer"
-                            />
+                            <p className="text-slate-200 mb-20 mt-10">
+                                Indiquez votre adresse e-mail pour recevoir un message permettant
+                                d'obtenir votre mot de passe
+                            </p>
                         </div>
-                    </div>
+                        <form onSubmit={handleForgottenPassword as any}>
+                            <div className="flex flex-nowrap">
+                                <div className="mr-10">
+                                    <Input
+                                        onChange={e => setIdentifer(e)}
+                                        placeholder="john.doe@pinotes.com"
+                                        size="large"
+                                    />
+                                </div>
+                                <div>
+                                    <Button
+                                        isLoading={isLoading}
+                                        onClick={(e: any) => handleForgottenPassword(e)}
+                                        title="Envoyer"
+                                    />
+                                </div>
+                            </div>
 
-                    <div className="text-red-800 mb-3">{error}</div>
-                </form>
+                            <div className="text-red-800 mb-3">{error}</div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
