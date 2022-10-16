aa854a023dc0bef0944f12b280d2219c27153357
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-16 03:20:24 +0200

    display/create notes and folders tree

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { AiFillCaretRight, AiFillFolderAdd } from 'react-icons/ai'
 import { GrFormClose } from 'react-icons/gr'
+import { FaRegStickyNote } from 'react-icons/fa'
 import { BsCheck } from 'react-icons/bs'
 import { toast } from 'react-toastify'
 
@@ -17,26 +18,27 @@ import Button from './Button'
 
 interface FolderProps {
     folders?: IFolder[]
-    parentId?: string
     id: string
     title: string
     notes?: INote[]
 }
 
-const Folder: React.FC<FolderProps> = ({ folders, parentId = undefined, id, title, notes }) => {
+const Folder: React.FC<FolderProps> = ({ folders, id, title, notes }) => {
     const { user } = useContext(AuthContext)
 
     const [isActive, setIsActive] = useState(true)
+
     const [newFolder, setNewFolder] = useState('')
     const [isNewFolder, setIsNewFolder] = useState(false)
+    const [newNote, setNewNote] = useState('')
+    const [isNewNote, setIsNewNote] = useState(false)
 
     const createFolder = async () => {
-        setIsNewFolder(false)
-
+        setIsActive(true)
         try {
             const { data } = await api.post(urls.API.CREATE_FOLDER, {
                 title: newFolder,
-                parentId: parentId,
+                parentId: id,
                 userId: user?.id,
             })
             toast('Dossier créé avec succès !', { type: 'success' })
@@ -48,14 +50,36 @@ const Folder: React.FC<FolderProps> = ({ folders, parentId = undefined, id, titl
         }
     }
 
+    const createNote = async () => {
+        setIsActive(true)
+        try {
+            const { data } = await api.post(urls.API.CREATE_NOTES, {
+                title: newNote,
+                folderId: id,
+                userId: user?.id,
+                text: newNote,
+            })
+            toast('Note créé avec succès !', { type: 'success' })
+        } catch (error) {
+            toast('Erreur lors de la création de la note', { type: 'warning' })
+        } finally {
+            setIsNewNote(false)
+            setNewNote('')
+        }
+    }
+
     return (
         <div className="pl-2 mb-3">
-            <div
-                className="flex justify-between items-center cursor-pointer mb-1"
-                onClick={() => setIsActive(prev => !prev)}
-            >
-                <span className="font-bold">{title}</span>
+            <div className="flex justify-between items-center cursor-pointer mb-1">
+                <span className="font-bold" onClick={() => setIsActive(prev => !prev)}>
+                    {title}
+                </span>
                 <div className="flex items-center">
+                    <Button
+                        Icon={<FaRegStickyNote size={14} />}
+                        onClick={() => setIsNewNote(true)}
+                        noBg
+                    />
                     <Button
                         Icon={<AiFillFolderAdd size={16} />}
                         onClick={() => setIsNewFolder(true)}
@@ -63,12 +87,50 @@ const Folder: React.FC<FolderProps> = ({ folders, parentId = undefined, id, titl
                     />
                     <AiFillCaretRight
                         className={`${isActive && 'rotate-90'} ml-2 transition-all`}
+                        onClick={() => setIsActive(prev => !prev)}
                     />
                 </div>
             </div>
-            {notes?.map((note: INote) => (
-                <Note key={note._id} title={note.title} text={note.text} state={note.state} />
-            ))}
+            <div
+                className={`${
+                    !isNewNote && 'hidden'
+                } relative ml-2 mb-1 transition-all duration-500`}
+            >
+                <input
+                    className="w-full p-1 bg-blue-700 rounded-md"
+                    type="text"
+                    value={newNote}
+                    onChange={e => setNewNote(e.target.value)}
+                />
+                <div className="absolute top-2 right-2 flex">
+                    <GrFormClose
+                        color="red"
+                        size={16}
+                        className="cursor-pointer"
+                        onClick={() => {
+                            setIsNewNote(false)
+                            setNewNote('')
+                        }}
+                    />
+                    <BsCheck
+                        color="white"
+                        size={16}
+                        className="cursor-pointer"
+                        onClick={() => createNote()}
+                    />
+                </div>
+            </div>
+            {isActive &&
+                notes
+                    ?.filter((note: INote) => id === note?.folderId)
+                    .map((note: INote) => (
+                        <Note
+                            key={note._id}
+                            title={note.title}
+                            text={note.text}
+                            state={note.state}
+                        />
+                    ))}
             <div
                 className={`${
                     !isNewFolder && 'hidden'
@@ -98,26 +160,26 @@ const Folder: React.FC<FolderProps> = ({ folders, parentId = undefined, id, titl
                     />
                 </div>
             </div>
-            {folders?.map((folder: IFolder) => {
-                if (folder.parentId === id) {
-                    const notesFiltered = notes?.filter(
-                        (note: INote) => folder?._id === note?.folderId,
-                    )
+            {isActive &&
+                folders?.map((folder: IFolder) => {
+                    if (folder.parentId === id) {
+                        const notesFiltered = notes?.filter(
+                            (note: INote) => folder?._id === note?.folderId,
+                        )
 
-                    return (
-                        <Folder
-                            key={folder._id}
-                            title={folder.title}
-                            parentId={folder.parentId}
-                            id={folder._id}
-                            folders={folders}
-                            notes={notesFiltered}
-                        />
-                    )
-                } else {
-                    return
-                }
-            })}
+                        return (
+                            <Folder
+                                key={folder._id}
+                                title={folder.title}
+                                id={folder._id}
+                                folders={folders}
+                                notes={notesFiltered}
+                            />
+                        )
+                    } else {
+                        return
+                    }
+                })}
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { AiFillFolderAdd } from 'react-icons/ai'
+import { GrFormClose } from 'react-icons/gr'
+import { BsCheck } from 'react-icons/bs'
+import { toast } from 'react-toastify'
 
 import { IFolder } from '../types/IFolder'
 import { INote } from '../types/INote'
+
+import api from '../helpers/api'
+import { urls } from '../helpers/urls'
 
 import AuthContext from '../contexts/AuthContext'
 
 import ProfileCard from './ProfileCard'
 import Folder from './Folder'
+import Button from './Button'
 
 interface NavbarProps {
     folders?: IFolder[]
@@ -19,7 +27,24 @@ const Navbar: React.FC<NavbarProps> = ({ folders, notes }) => {
 
     const { user } = useContext(AuthContext)
 
-    console.log({ folders }, { notes })
+    const [newFolder, setNewFolder] = useState('')
+    const [isNewFolder, setIsNewFolder] = useState(false)
+
+    const createFolder = async () => {
+        try {
+            const { data } = await api.post(urls.API.CREATE_FOLDER, {
+                title: newFolder,
+                parentId: undefined,
+                userId: user?.id,
+            })
+            toast('Dossier créé avec succès !', { type: 'success' })
+        } catch (error) {
+            toast('Erreur lors de la création du dossier', { type: 'warning' })
+        } finally {
+            setIsNewFolder(false)
+            setNewFolder('')
+        }
+    }
 
     return (
         <nav className="p-2 w-80 h-full flex flex-col justify-between bg-blue-900">
@@ -30,30 +55,60 @@ const Navbar: React.FC<NavbarProps> = ({ folders, notes }) => {
                         {user?.isPremium ? IM_PREMIUM : BE_PREMIUM}
                     </span>
                 </div>
+                <div className="my-4">
+                    <Button
+                        Icon={<AiFillFolderAdd size={16} />}
+                        onClick={() => setIsNewFolder(true)}
+                        noBg
+                    />
+                </div>
             </div>
             <div className="flex flex-col flex-1">
                 <div className="text-slate-50">
-                    {
-                        <div className="flex flex-col">
-                            {folders?.map((folder: IFolder) => {
-                                if (!folder.parentId) {
-                                    const notesFiltered = notes?.filter(
-                                        (note: INote) => folder?._id === note?.folderId,
-                                    )
-                                    return (
-                                        <Folder
-                                            title={folder.title}
-                                            folders={folders}
-                                            id={folder._id}
-                                            parentId={folder._id}
-                                            key={folder._id}
-                                            notes={notesFiltered}
-                                        />
-                                    )
-                                }
-                            })}
+                    <div
+                        className={`${
+                            !isNewFolder && 'hidden'
+                        } relative ml-2 mb-2 transition-all duration-500`}
+                    >
+                        <input
+                            className="w-full p-1 bg-blue-700 rounded-md"
+                            type="text"
+                            value={newFolder}
+                            onChange={e => setNewFolder(e.target.value)}
+                        />
+                        <div className="absolute top-2 right-2 flex">
+                            <GrFormClose
+                                color="red"
+                                size={16}
+                                className="cursor-pointer"
+                                onClick={() => {
+                                    setIsNewFolder(false)
+                                    setNewFolder('')
+                                }}
+                            />
+                            <BsCheck
+                                color="white"
+                                size={16}
+                                className="cursor-pointer"
+                                onClick={() => createFolder()}
+                            />
                         </div>
-                    }
+                    </div>
+                    <div className="flex flex-col">
+                        {folders?.map((folder: IFolder) => {
+                            if (!folder.parentId) {
+                                return (
+                                    <Folder
+                                        title={folder.title}
+                                        folders={folders}
+                                        id={folder._id}
+                                        key={folder._id}
+                                        notes={notes}
+                                    />
+                                )
+                            }
+                        })}
+                    </div>
                 </div>
             </div>
             <ProfileCard />
