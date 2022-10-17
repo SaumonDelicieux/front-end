340ad46fc11eba57a10064f68587a92d56ba68b1
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-17 17:09:31 +0200

    add delete and create folder function

--- src/features/folders/foldersSlice.ts
+++ src/features/folders/foldersSlice.ts
@@ -2,9 +2,10 @@ import { createSlice } from '@reduxjs/toolkit'
 
 import { IFoldersState } from '../../types/states/IFoldersState'
 
-import { getAllFolders } from '../../actions/folders'
+import { createFolder, deleteFolder, getAllFolders } from '../../actions/folders'
 
 import type { RootState } from '../../store'
+import { IFolder } from 'src/types/IFolder'
 
 const initialState: IFoldersState = {
     folders: [],
@@ -24,6 +25,26 @@ export const foldersSlice = createSlice({
             })
             .addCase(getAllFolders.fulfilled, (state, { payload }) => {
                 state.folders = payload.folders
+                state.loading = false
+                state.error = ''
+            })
+            .addCase(createFolder.pending, state => {
+                state.loading = true
+                state.error = ''
+            })
+            .addCase(createFolder.fulfilled, (state, { payload }) => {
+                state.folders = state.folders?.concat(payload.folder)
+                state.loading = false
+                state.error = ''
+            })
+            .addCase(deleteFolder.pending, state => {
+                state.loading = true
+                state.error = ''
+            })
+            .addCase(deleteFolder.fulfilled, (state, { payload }) => {
+                state.folders = state.folders?.filter(
+                    (folder: IFolder) => folder._id != payload.folderId,
+                )
                 state.loading = false
                 state.error = ''
             })
