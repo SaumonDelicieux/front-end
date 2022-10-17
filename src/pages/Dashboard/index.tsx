9a8afad3b32fabfaa34ae5b51d483129fffc2fb1
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-17 04:03:18 +0200

    init note selected page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { useAppSelector } from '../../hooks'
+
 const Dashboard: React.FC = () => {
+    const { selectedNote } = useAppSelector(state => state.notes)
+
     return (
         <div className="flex-1 p-2">
-            <div>hello</div>
+            <div className="mb-10 text-2xl font-bold">{selectedNote?.title}</div>
+            <div>{selectedNote?.text}</div>
         </div>
     )
 }
