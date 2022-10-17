87985dc240732be0e996223b36548e676667f2ca
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-17 04:00:13 +0200

    add state

---
+++ src/types/states/IUserState.ts
@@ -0,0 +1,10 @@
+export interface IUserState {
+    id?: string
+    email?: string
+    firstName?: string
+    lastName?: string
+    isPremium?: boolean
+    token?: string
+    loading?: boolean
+    error?: string
+}
