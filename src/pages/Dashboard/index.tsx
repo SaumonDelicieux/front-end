48209b4b0dec512d22b37a4fc972d9115b171cbd
Lamine ADEL <lamine.adel@ynov.com>
2022-10-20 15:17:02 +0200

    firstone

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,13 @@
-import React from "react"
-
+import React, { useState } from "react"
+import TextArea from "../../components/TextArea"
 import { useAppSelector } from "../../hooks"
 
 const Dashboard: React.FC = () => {
     const { selectedNote } = useAppSelector(state => state.notes)
-
     return (
         <div className="flex-1 p-2">
             <div className="mb-10 text-2xl font-bold">{selectedNote?.title}</div>
-            <div>{selectedNote?.text}</div>
+            <TextArea />
         </div>
     )
 }
