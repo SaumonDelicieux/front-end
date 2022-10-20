3be47731a91e7bd4578864abc18d790b4489f110
Melly Lucas <melly.lucas32@yahoo.fr>
2022-10-20 12:39:40 +0200

    Wysiwyg

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,48 +1,120 @@
-import React, { ChangeEvent } from "react"
+import React, { useState } from "react"
+import {
+    AiOutlineAlignRight,
+    AiOutlineAlignLeft,
+    AiOutlineAlignCenter,
+    AiOutlineBold,
+    AiOutlineItalic,
+    AiOutlinePaperClip,
+    AiOutlineOrderedList,
+    AiOutlineUnorderedList,
+    AiFillCaretDown,
+} from "react-icons/ai"
 
-import { updateBold } from "../helpers/WYSIWYG/updateBold"
-
-import { sizeText, SizeType } from "../helpers/WYSIWYG/sizeText"
+import Button from "../components/Button"
 
 interface ContentProps {
     text?: string
 }
 
 const Content: React.FC<ContentProps> = ({ text }) => {
-    const handleSizeText = (e: ChangeEvent<HTMLSelectElement>) => {
-        e.preventDefault()
-
-        // sizeText("rfr", e.target.value as SizeType)
-        updateBold()
-    }
-
-    const show = () => {
-        if (window) {
-            const selectedText = window.getSelection()
-            console.log(selectedText)
-        }
-    }
+    const [textAlignRight, setTextAlignRight] = useState(false)
+    const [textAlignLeft, setTextAlignLeft] = useState(false)
+    const [textAlignCenter, setTextAlignCenter] = useState(false)
 
     return (
-        <div>
-            <div className="mb-5">
-                <button onClick={(e: any) => handleSizeText(e)}>okok</button>
-                <select
-                    name="sizeTextChoice"
-                    id="sizeTextChoice"
-                    className="p-2 text-black rounded-md"
-                    onChange={e => handleSizeText(e)}
-                >
-                    <option value="H1">H1</option>
-                    <option value="H2">H2</option>
-                    <option value="H3">H3</option>
-                    <option value="H4">H4</option>
-                    <option value="H5">H5</option>
-                    <option value="H6">H6</option>
-                </select>
-            </div>
-            <div contentEditable className="border p-2 rounded-md focus:outline-none">
-                {text}
+        <div className="flex-1 p-2 ml-5 mr-5">
+            <div
+                className={
+                    textAlignRight
+                        ? "text-right ..."
+                        : textAlignLeft
+                        ? "text-right ..."
+                        : textAlignCenter
+                        ? "text-center ..."
+                        : ""
+                }
+            >
+                <div className="flex">
+                    <Button title="Publier" onClick={() => console.log("")} />
+                    <Button title="Partager" onClick={() => console.log("")} />
+                    <Button title="Download" onClick={() => console.log("")} />
+                    <Button title="Archiver" onClick={() => console.log("")} />
+                </div>
+
+                <span className="flex bg-slate-200 p-1 ... rounded-md mt-20">
+                    <div className="flex border border-slate-900 ... ml-1 mr-1">
+                        <select
+                            className="text-slate-900 text-xs bg-slate-200"
+                            name="typography"
+                            id="typography"
+                        >
+                            <option
+                                value="calibri"
+                                onClick={() => {
+                                    console.log("typographie calibri")
+                                }}
+                            >
+                                Calibri
+                            </option>
+                            <option value="timesNewRoman">Times new roman</option>
+                            <option value="arial">Arial</option>
+                        </select>
+                    </div>
+                    <div className="flex border border-slate-900 ... ml-1 mr-1">
+                        <select
+                            className="text-slate-900 text-xs bg-slate-200"
+                            name="size"
+                            id="size"
+                        >
+                            <option
+                                value="10"
+                                onClick={() => {
+                                    console.log("taille 10")
+                                }}
+                            >
+                                10
+                            </option>
+                            <option value="14">14</option>
+                            <option value="18">18</option>
+                            <option value="24">24</option>
+                        </select>
+                    </div>
+                    <AiOutlineBold
+                        className="text-slate-900 mt-0.5"
+                        onClick={() => console.log("gras")}
+                    />
+
+                    <AiOutlineItalic
+                        className="text-slate-900 mr-1 mt-0.5"
+                        onClick={() => console.log("italic")}
+                    />
+                    <AiOutlineOrderedList
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => console.log("mettre des puce 1 2 3")}
+                    />
+                    <AiOutlineUnorderedList
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => console.log("mettre des puce . . .")}
+                    />
+                    <AiOutlineAlignLeft
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => console.log("text align left")}
+                    />
+                    <AiOutlineAlignCenter
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => console.log("text align center")}
+                    />
+                    <AiOutlineAlignRight
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => setTextAlignRight(true)}
+                    />
+                    <AiOutlinePaperClip
+                        className="text-slate-900 mr-2 mt-0.5"
+                        onClick={() => console.log("importer une image")}
+                    />
+                </span>
+                <div>Note : {text}</div>
             </div>
         </div>
     )
