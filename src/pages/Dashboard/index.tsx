3333e76f96e9b03ae0ada062e672bd54fb138638
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-20 17:08:24 +0200

    dynamic display

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react"
-import TextArea from "../../components/TextArea"
+import React from "react"
+
+import Content from "../../components/Content"
+
 import { useAppSelector } from "../../hooks"
 
 const Dashboard: React.FC = () => {
     const { selectedNote } = useAppSelector(state => state.notes)
+
+    if (selectedNote) {
+        return (
+            <div className="flex-1 p-2">
+                <div className="mb-10 text-2xl font-bold">{selectedNote?.title}</div>
+                <Content text={selectedNote?.text} />
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1 p-2">
-            <div className="mb-10 text-2xl font-bold">{selectedNote?.title}</div>
-            <TextArea />
+            <h1>📝 Selectionner une note</h1>
         </div>
     )
 }
