8d90a8c0d11ed4eca61fd625539880dff0f321df
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-21 16:14:15 +0200

    little update

--- src/helpers/convertToPDF.ts
+++ src/helpers/convertToPDF.ts
@@ -3,6 +3,7 @@ import { jsPDF } from "jspdf"
 export const convertToPDF = (title: string, content = "") => {
     const doc = new jsPDF()
 
+    doc.text(title, 10, 10)
     doc.text(content, 10, 10)
     doc.save(`${title}.pdf`)
 }
