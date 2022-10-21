5f9cc09142f9317ec1990f58a48e71cb7e781c5e
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-21 12:32:13 +0200

    fix mistake

--- src/helpers/WYSIWYG/addParagraph.ts
+++
@@ -1,7 +0,0 @@
-export const addParagraphTag = evt => {
-    if (evt.keyCode == "13" && window) {
-        // don't add a p tag on list item
-        if (window?.getSelection()?.anchorNode.parentNode.tagName === "LI") return
-        document.createRange("formatBlock", false, "p")
-    }
-}
