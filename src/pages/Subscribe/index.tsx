1c27edcdb63a6211c26bac18a61f67bc96b42bca
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-21 14:43:29 +0200

    flex me instead of grid

--- src/pages/Subscribe/index.tsx
+++ src/pages/Subscribe/index.tsx
@@ -13,8 +13,8 @@ const Subscribe: React.FC = () => {
     const { isPremium, error, loading } = useAppSelector(state => state.user)
 
     return (
-        <div className="w-screen h-screen relative bg-slate-900 text-p-2 text-base transition-colors">
-            <div className="pl-14 pt-14">
+        <div className="w-full h-full relative bg-slate-200 dark:bg-slate-900 text-p-2 text-base transition-colors">
+            <div className="absolute top-14 left-14">
                 <Button
                     icon={<AiFillCaretLeft color="white" size={20} />}
                     onClick={() => !isPremium && navigate(urls.APP.DASHBOARD)}
@@ -22,16 +22,14 @@ const Subscribe: React.FC = () => {
                 />
             </div>
             <div className="flex justify-center items-center w-full h-full">
-                <div className=" grid grid-cols-2 gap-4 w-8/12">
-                    <div className="flex-auto justify-items-center bg-slate-900">
-                        <div className="text-slate-200 text-center text-2xl py-8">
-                            Offre gratuite
-                        </div>
-                        <div className="text-slate-200 text-xl py-8 px-16">
+                <div className="flex flex-col sm:flex-row p-12">
+                    <div className="flex-auto justify-items-center bg-slate-200 dark:bg-slate-900 text-blue-800 dark:text-slate-200">
+                        <div className="text-center text-2xl py-8">Offre gratuite</div>
+                        <div className="text-xl py-8 px-16">
                             Grâce à votre profil gratuit vous bénéfier d'un profil personnel sur
                             lequel vous pouvez créer vos notes.
                         </div>
-                        <div className="text-slate-200 px-16">
+                        <div className="px-16">
                             <ul className="marker:text-green list-outside list-disc ml-4">
                                 <li className="py-3">Insertion d'images et de fichier</li>
                                 <li className="py-3">Téléchargeable en pdf</li>
