8f05062e70ddda4f47a8a5a49289ac4ac639f52d
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-10-21 10:06:07 +0200

    fix error

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactLoading from "react-loading"
 
 interface ButtonProps {
-    onClick: any
+    onClick?: any
     title?: string
     type?: "submit" | "button"
     isLoading?: boolean
