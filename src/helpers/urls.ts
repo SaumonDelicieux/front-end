fe526a92009ba7ce43687b809e29104e31da5ac0
Walid <mokhtariwalid@hotmail.com>
2022-10-21 11:27:11 +0200

    Fix subscribe module

--- src/helpers/urls.ts
+++ src/helpers/urls.ts
@@ -5,7 +5,7 @@ export const urls = {
         FORGOTTEN_PASSWORD: "/forgottenPassword",
         DASHBOARD: "/",
         PROFILE: "/profile",
-        SUBSCRIPTION: "/subscription",
+        SUBSCRIBE: "/subscribe",
     },
     API: {
         LOGIN: "/users/login",
