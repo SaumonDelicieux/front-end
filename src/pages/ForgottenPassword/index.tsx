f6d63d3e603da618daff82acb3d757e26eaa844f
Enzo Candotti <enzo.candotti16@hotmail.com>
2022-11-03 16:42:55 +0100

    refacto css

--- src/pages/ForgottenPassword/index.tsx
+++ src/pages/ForgottenPassword/index.tsx
@@ -24,41 +24,40 @@ const ForgottenPassword: React.FC = () => {
     }
 
     return (
-        <div className="w-screen h-screen relative bg-blue-400 dark:bg-slate-900 text-p-2 text-base transition-colors">
-            <div>
-                <Button icon={<IoIosArrowBack />} onClick={() => navigate(urls.APP.LOGIN)} />
-                <div className="mt-20 pt-10">
-                    <div className="w-full h-full flex flex-col items-center justify-center">
-                        <div className="flex flex-col justify-center items-center mb-10 font-bold text-2xl select-none">
-                            <div>
-                                <span className="mr-2 text-slate-200">Mot de passe oublié ?</span>
-                            </div>
-                        </div>
+        <div className="w-full text-p-2 text-base">
+            <Button icon={<IoIosArrowBack />} onClick={() => navigate(urls.APP.LOGIN)} />
+            <div className="mt-20 pt-10">
+                <div className="w-full h-full flex flex-col items-center justify-center">
+                    <div className="flex flex-col justify-center items-center mb-10 font-bold text-2xl select-none">
                         <div>
-                            <p className="text-slate-200 my-10">
-                                Indiquez votre adresse e-mail ou votre numéro de téléphone pour
-                                recevoir un lien de réinitialisation de mot de passe dans votre
-                                boîte mail
-                            </p>
+                            <span className="mr-2">Mot de passe oublié ?</span>
                         </div>
-                        <form onSubmit={handleForgottenPassword as any}>
-                            <div className="flex flex-col items-center">
-                                <Input
-                                    label="Adresse mail ou numéro de téléphone"
-                                    onChange={e => setIdentifer(e)}
-                                    placeholder="060606606"
-                                    size="large"
-                                />
-                                <Button
-                                    isLoading={loading}
-                                    onClick={(e: any) => handleForgottenPassword(e)}
-                                    title="Envoyer"
-                                />
-                            </div>
-
-                            <div className="text-red-800 mb-3">{error}</div>
-                        </form>
                     </div>
+                    <div>
+                        <p className="my-10">
+                            Indiquez votre adresse e-mail ou votre numéro de téléphone pour recevoir
+                            un lien de réinitialisation de mot de passe dans votre boîte mail
+                        </p>
+                    </div>
+                    <form onSubmit={handleForgottenPassword as any}>
+                        <div className="flex flex-col items-center">
+                            <Input
+                                label="Adresse mail ou numéro de téléphone"
+                                onChange={e => setIdentifer(e)}
+                                placeholder="060606606"
+                                size="large"
+                                className="mb-3"
+                            />
+                            <Button
+                                isLoading={loading}
+                                onClick={(e: any) => handleForgottenPassword(e)}
+                                title="Envoyer"
+                                className="border rounded-md p-2 bg-slate-100 dark:text-slate-900"
+                            />
+                        </div>
+
+                        <div className="text-red-800 mb-3">{error}</div>
+                    </form>
                 </div>
             </div>
         </div>
