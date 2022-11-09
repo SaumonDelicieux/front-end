44a658bef97ce50007fc309262e1fdd2b191455d
Walid <mokhtariwalid@hotmail.com>
2022-11-10 00:52:48 +0100

    last commit

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { toast } from "react-toastify"
+import AsyncSelect from "react-select/async"
+
+import { AiOutlineClose } from "react-icons/ai"
+import { TiDelete } from "react-icons/ti"
 
 import { urls } from "../helpers/urls"
 import api from "../helpers/api"
@@ -89,6 +93,12 @@ const Popup: React.FC = () => {
                 type="text"
                 placeholder="E.g joe"
                 onChange={e => setSearch(e.target.value)}
+        <div className="rounded-md bg-slate-400 dark:bg-blue-900 w-6/12 h-3/4 fixed top-40 right-80">
+            <AiOutlineClose
+                color="#e74c3c"
+                size={25}
+                className="cursor-pointer absolute right-0 mt-2 mr-2 text-red-500"
+                onClick={() => dispatch(changeOnShareState())}
             />
             <div className="flex">
                 <input
@@ -103,6 +113,33 @@ const Popup: React.FC = () => {
                     value="Annuler"
                     onClick={() => dispatch(changeOnPublishState())}
                 />
+                <div className="flex">
+                    <input
+                        className="cursor-pointer bg-green-700 p-3 rounded-md mt-3"
+                        type="submit"
+                        value="Partager"
+                        onClick={() => {
+                            submitUsers()
+                        }}
+                    />
+                </div>
+            </div>
+            <h2 className="ml-2">Pariticipants :</h2>
+            <div className="w-6/12 h-2/4 fixed overflow-y-scroll">
+                {sharedWithList.map((userNames, index) => (
+                    <div
+                        key={index}
+                        className="flex items-center justify-between m-3 pb-2 border-solid border-b-2"
+                    >
+                        <p>{userNames.userName}</p>
+                        <TiDelete
+                            color="#e74c3c"
+                            size={30}
+                            className="cursor-pointer"
+                            onClick={() => deleteUserFromSharedList(userNames.userId)}
+                        />
+                    </div>
+                ))}
             </div>
             {usersToSuggest.map((user: any, index: number) => (
                 <p
